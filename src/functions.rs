fa1e89d1759bf7e747d8cb44cc5a0499fb5ac542
arwysyah <arwysyahputrasiregar@gmail.com>
2024-02-12 14:52:58 +0700

    add functions

---
+++ src/functions.rs
@@ -0,0 +1,23 @@
+
+pub fn print_function (){
+
+another_function();
+
+    pass_int_param(13);
+
+let result:i32 =    return_int();
+    println!("here is return in values :{result}")
+
+}
+
+fn pass_int_param (arg:i32){
+
+    println!("here is the params {arg}");
+}
+
+fn return_int ()-> i32{
+    return 12;
+}
+fn another_function (){
+println!("another function");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use crate::{constant::print_constant, types::print_data_types};
+use crate::{constant::print_constant, functions::print_function, types::print_data_types};
 
 mod constant;
 mod types;
+mod functions;
 fn main() {
     println!("Hello, world!");
     print_constant();
     print_data_types();
+    print_function();
 }
